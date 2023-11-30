69318ab2683f6ec4b936543bcd6657d662ab0add
Helder Mauricio Da Silva Cabo <heda-sil@student.42porto.com>
2023-11-30 12:30:45 +0000

    Changed values for mouse movement

--- bonus/srcs/mouse.c
+++ bonus/srcs/mouse.c
@@ -6,7 +6,7 @@
 /*   By: heda-sil <heda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:32:13 by heda-sil          #+#    #+#             */
-/*   Updated: 2023/11/29 17:21:34 by heda-sil         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:30:13 by heda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,25 @@ void	ft_mouse_rotate(double olddirx, double oldplanex, double ang, t_data *g)
 	g->planey = oldplanex * sin(ang) + g->planey * cos(ang);
 }
 
-// TODO: Need to improve view moviment, some performance trouble, only works if mouse is moving
 int	mouse(int x, int y, t_data *g)
 {
 	int		dx;
 
-	// mlx_mouse_hide(g->mlx, g->mlx_window); // TMP: Moved to main
-	if (x < W_WIDTH / 4)
-		mlx_mouse_move(g->mlx, g->mlx_window, x + W_WIDTH / 4, y);
-	if (x > (3 * W_WIDTH) / 4)
-		mlx_mouse_move(g->mlx, g->mlx_window, x - W_WIDTH / 4, y);
+	if (x < W_WIDTH / 6)
+		mlx_mouse_move(g->mlx, g->mlx_window, x + W_WIDTH / 6, W_HEIGHT / 2);
+	if (x > (5 * W_WIDTH) / 6)
+		mlx_mouse_move(g->mlx, g->mlx_window, x - W_WIDTH / 6, W_HEIGHT / 2);
+	if (y < W_HEIGHT / 4 || y > (3 * W_HEIGHT) / 4)
+		mlx_mouse_move(g->mlx, g->mlx_window, x, W_HEIGHT / 2);
 	dx = g->old_x - x;
 	g->old_x = x;
 	if (dx > 0)
 	{
-		ft_mouse_rotate(g->inidirx, g->planex, -0.05, g);
+		ft_mouse_rotate(g->inidirx, g->planex, -0.03, g);
 	}
 	else
 	{
-		ft_mouse_rotate(g->inidirx, g->planex, 0.05, g);
+		ft_mouse_rotate(g->inidirx, g->planex, 0.03, g);
 	}
-	ft_raycast(g);
 	return (0);
 }
